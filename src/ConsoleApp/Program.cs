f848f0d28fb57fb1f8bfc6c3361d935fd26ec0aa
jeffery li <jeffery.li@grapecity.com>
2021-03-22 16:24:13 +0800

    initialize the new draftbook

---
+++ src/ConsoleApp/Program.cs
@@ -0,0 +1,20 @@
+﻿using MultiThread;
+using System;
+
+namespace ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MultiThread();
+            Console.ReadKey();
+        }
+
+        static void MultiThread()
+        {
+            var foo = new MultiPractice();
+            foo.Demo1();
+        }
+    }
+}
---
+++ src/MultiThread/MultiPractice.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MultiThread
+{
+    public class MultiPractice
+    {
+        private int _count1;
+
+        public void Demo1()
+        {
+            var tasks = new Task[10];
+            for (var i = 0; i < 10; i++)
+            {
+                tasks[i] = new Task(Count1);
+            }
+
+            Task.WhenAll(tasks);
+            Console.WriteLine($"the current count is: {_count1}");
+        }
+
+        private void Count1()
+        {
+            for (var j = 0; j < 1000; j++)
+            {
+                _count1++;
+            }
+        }
+    }
+}
